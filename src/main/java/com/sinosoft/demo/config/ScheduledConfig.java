10d9f9f8564d4d934c4efe8c4150cc502b94f4e0
hiYuzu <pomelo.yuzu1997@gmail.com>
2022-08-14 13:22:17 +0800

    update 0814

---
+++ src/main/java/com/sinosoft/demo/config/ScheduledConfig.java
@@ -0,0 +1,20 @@
+package com.sinosoft.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+/**
+ * @author hiYuzu
+ * @version v1.0
+ * @date 2022/8/11 12:13
+ */
+@Configuration
+public class ScheduledConfig implements SchedulingConfigurer {
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        scheduledTaskRegistrar.setScheduler(new ScheduledThreadPoolExecutor(50));
+    }
+}
---
+++ src/main/java/com/sinosoft/demo/pojo/CountryNucleic.java
@@ -0,0 +1,54 @@
+package com.sinosoft.demo.pojo;
+
+/**
+ * @author hiYuzu
+ * @version v1.0
+ * @date 2022/8/14 12:24
+ */
+public class CountryNucleic {
+    private String zjhm;
+    private String xm;
+    private String hsjcjg;
+    private String hsjcsj;
+    private String hsjcjgmc;
+
+    public String getZjhm() {
+        return zjhm;
+    }
+
+    public void setZjhm(String zjhm) {
+        this.zjhm = zjhm;
+    }
+
+    public String getXm() {
+        return xm;
+    }
+
+    public void setXm(String xm) {
+        this.xm = xm;
+    }
+
+    public String getHsjcjg() {
+        return hsjcjg;
+    }
+
+    public void setHsjcjg(String hsjcjg) {
+        this.hsjcjg = hsjcjg;
+    }
+
+    public String getHsjcsj() {
+        return hsjcsj;
+    }
+
+    public void setHsjcsj(String hsjcsj) {
+        this.hsjcsj = hsjcsj;
+    }
+
+    public String getHsjcjgmc() {
+        return hsjcjgmc;
+    }
+
+    public void setHsjcjgmc(String hsjcjgmc) {
+        this.hsjcjgmc = hsjcjgmc;
+    }
+}
